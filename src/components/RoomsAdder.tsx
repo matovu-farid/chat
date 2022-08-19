05d5d61e7bfb6e8fb606ed74cb7413e94019c6b2
matovu-farid <matovu90@gmail.com>
2022-08-20 01:33:11 +0300

    Add a room adder

---
+++ src/components/RoomsAdder.tsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { trpc } from "../utils/trpc";
+import { AiFillPlusCircle, AiOutlineClose } from "react-icons/ai";
+
+import User from "../Interfaces/User";
+import { useRouter } from "next/router";
+import ConfirmBox from "./ConfirmBox";
+import Modal from "./Modal";
+
+
+const RoomAdder = () => {
+  const [search, setSearch] = useState("");
+  const { data: users } = trpc.useQuery(["user.searchUser", search]);
+  const router = useRouter();
+  const { roomId } = router.query;
+  const {mutate:addToRoom} = trpc.useMutation(["room.addToRoom"]);
+
+  const handleUserClicked = (user: User) => {
+    if (typeof roomId === "string" && user)
+    addToRoom({ userId: user.id, roomId });
+  };
+  const [showPopup, setShowPopup] = useState(false);
+
+  return (
+    <Modal>
+      {showPopup || (
+        <AiFillPlusCircle
+          onClick={() => setShowPopup(true)}
+          className="fixed left-6 top-[15%] text-4xl z-50 text-gray-900 hover:cursor-pointer hover:scale-125 hover:text-purple-600 active:text-purple-600 transition-all "
+        ></AiFillPlusCircle>
+      )}
+      {showPopup && (
+        <div className=" flex gap-2 fixed left-6 top-[15%]">
+          <AiOutlineClose
+            onClick={() => setShowPopup(false)}
+            className="absolute right-2 top-2 cursor-pointer"
+          ></AiOutlineClose>
+          <div>
+            <input
+              type="text"
+              value={search}
+              placeholder="search user..."
+              onChange={(e) => setSearch(e.target.value)}
+              className=" border-none p-2 text-gray-900 rounded-lg outline-none"
+            />
+
+            <div>
+              {users && users.length > 0 && (
+                <ul className="bg-white text-gray-900 flex flex-col rounded-lg p-2 shadow-md">
+                  {users?.map((user) => (
+                    <button
+                      onClick={() => handleUserClicked(user)}
+                      className="transition-colors my-0 px-2 rounded-lg cursor-pointer hover:bg-gray-900 hover:text-white active:text-white"
+                      key={user.id}
+                    >
+                      <div className="flex py-1 justify-start">
+                        <div className="w-[90%]">{user.name}</div>
+                        <div className="w-[10%] hover:bg-green-600 rounded-sm flex flex-col justify-center items-center">
+                          <AiFillPlusCircle className=""></AiFillPlusCircle>
+                        </div>
+                      </div>
+                    </button>
+                  ))}
+                </ul>
+              )}
+
+              {search && users && users.length === 0 && (
+                <p className="bg-white text-gray-900 rounded-lg p-2">
+                  No users found
+                </p>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+    </Modal>
+  );
+};
+
+export default RoomAdder;
--- src/components/messege_components/MessageSection.tsx
+++ src/components/messege_components/MessageSection.tsx
@@ -5,6 +5,7 @@ import MessegeList from "./MessegeList";
 import useUser from "../../hooks/useUser";
 import Modal from "../Modal";
 import { AiFillPlusCircle } from "react-icons/ai";
+import RoomAdder from "../RoomsAdder";
 
 interface Props {
   roomId: string;
@@ -15,9 +16,7 @@ const MessegeSection = ({ roomId }: Props) => {
 
   return (
     <section className="w-full h-full flex flex-col justify-end">
-      <Modal>
-        <AiFillPlusCircle className="text-4xl z-50 text-gray-900 hover:cursor-pointer hover:text-purple-600 active:text-purple-600 transition-colors  fixed left-6 top-1/4"></AiFillPlusCircle>
-      </Modal>
+      <RoomAdder></RoomAdder>
       <MessegeList className="h-full" roomId={roomId}></MessegeList>
       <MessegeTextField
         className=" flex-none w-full"
