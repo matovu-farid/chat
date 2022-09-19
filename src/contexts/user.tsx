6eaa1ce4f0c1060765606edd365cff51557da45f
matovu-farid <matovu90@gmail.com>
2022-09-19 15:42:20 +0300

    Add preliminary welcome page

--- src/contexts/user.tsx
+++ src/contexts/user.tsx
@@ -1,35 +1,38 @@
 import { signIn, useSession } from "next-auth/react";
-import { Context, createContext, PropsWithChildren, useEffect, useState } from "react";
+import {
+  Context,
+  createContext,
+  PropsWithChildren,
+  useEffect,
+  useState,
+} from "react";
 import User from "../Interfaces/User";
+import router from "next/router";
 
-const dummyUser:User = {
-  id: '',
-  name: '',
-  email: '',
-  image: ''
-}
+const dummyUser: User = {
+  id: "",
+  name: "",
+  email: "",
+  image: "",
+};
 export const UserCtx: Context<User> = createContext(dummyUser);
 
- const UserProvider=({children}: PropsWithChildren)=>{
-  const [user, setUser] = useState<User>()
-  const {data, status} = useSession()
-  useEffect(()=>{
-    const userFromSession = data?.user
-    if(userFromSession && status==="authenticated"){
-      setUser(userFromSession)
+const UserProvider = ({ children }: PropsWithChildren) => {
+  const [user, setUser] = useState<User>();
+  const { data, status } = useSession();
+  useEffect(() => {
+    const userFromSession = data?.user;
+    if (userFromSession && status === "authenticated") {
+      setUser(userFromSession);
+    } else if (status === "unauthenticated") {
+      router.push("/");
     }
-    else if(status==="unauthenticated"){
-      signIn()
-    }
-  },[status])
+  }, [status]);
 
-  
-  return (
-    (user)?
-    <UserCtx.Provider value={user}>
-      {children}
-  </UserCtx.Provider>
-  : null
-  )
-}
+  return user ? (
+    <UserCtx.Provider value={user}>{children}</UserCtx.Provider>
+  ) : (
+    children
+  );
+};
 export default UserProvider;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,44 +1,66 @@
 import type { NextPage } from "next";
-import { signOut } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { BsFillTelephonePlusFill } from "react-icons/bs";
 
 import Link from "next/link";
 import useUser from "../hooks/useUser";
 
-
 const Home: NextPage = () => {
- 
   const buttonClasses = "bg-blue-500 text-white py-2 px-4 rounded-md";
- 
-  
+
   const handleSignout = () => {
     signOut();
   };
-  
-  const user = useUser()
-  
+
+  const user = useUser();
+  const { status } = useSession();
 
   return (
-    <>
-      { (
-        <>
-          <p>
-            You are signed in as {user.name} with {user.email}
-          </p>
-          <div className="flex gap-2">
-            <Link href={"/room/new"} >
-              <a className="bg-blue-500 py-2 px-3 rounded-md no-underline text-white">Create a Room</a>
-            </Link>
-               <Link href={"/room"} >
-              <a className="bg-blue-500 py-2 px-3 rounded-md no-underline text-white">Rooms</a>
-            </Link>
-            
-            <button onClick={handleSignout} className={buttonClasses}>
-              signout
-            </button>
+    <div className="grid place-items-center ">
+      <main className="grid justify-center max-w-[60em] text-center gap-2 ">
+        <h1 className="text-6xl">Welcome to Mafa </h1>
+        <h2 className="text-xl">
+          We're here to help you connect, communicate, and express your ideas so
+          you can get more done together. We offer organized spaces for everyone
+          and everything you need for work.
+        </h2>
+        {status == "authenticated" ? (
+          <div className="grid gap-4 justify-center">
+            <div>
+              <p>
+                {" "}
+                You are signed in as {user.name} with {user.email} email 👍
+              </p>
+              <p>We are happy to have you ❤️ </p>
+            </div>
+            <ol className="text-start list-decimal">
+              <li>
+                You can now search and call any user by clicking on the{"  "}
+                <BsFillTelephonePlusFill className="inline text-green-500 " />
+              </li>
+              <li>You can create rooms</li>
+              <li>You can chat with any user you have searched for</li>
+              <li>You can</li>
+            </ol>
           </div>
-        </>
-      ) }
-    </>
+        ) : (
+          <p className="text-lg">
+            <span>You can </span>
+            <span>
+              <button
+                className="bg-transparent text-blue-500"
+                onClick={() => {
+                  signIn();
+                }}
+              >
+                sign in
+              </button>
+            </span>
+            <span> here 😉 to get all access all the cool features</span>
+          </p>
+        )}
+      </main>
+    </div>
   );
 };
 
